e91249fa74e032550b8b401ac2f84ddd8d9fb624
likevarun <likevarun@users.noreply.github.com>
2021-01-01 18:08:09 +1100

    changes feature1

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.print(false);
+		System.out.print("Test");
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
 
